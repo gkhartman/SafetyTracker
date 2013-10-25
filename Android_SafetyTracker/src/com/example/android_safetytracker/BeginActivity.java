717932eae443116fc491ddbbfe5115de68d92dcf
jrsilva <Ruben@unknown7cd1c3e8cd4d.gateway.2wire.net>
2013-10-24 20:12:55 -0700

    JoseSilvaFirstCommit
    UI

---
+++ Android_SafetyTracker/src/com/example/android_safetytracker/BeginActivity.java
@@ -0,0 +1,56 @@
+package com.example.android_safetytracker;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+public class BeginActivity extends Activity implements View.OnClickListener{
+	
+	Button begin_Stop;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_begin);
+		
+		begin_Stop = (Button) findViewById(R.id.begin_StopButton);
+		begin_Stop.setOnClickListener(this);
+	}
+	
+	
+	
+	
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.begin, menu);
+		return true;
+	}
+
+
+
+
+
+	@Override
+	public void onClick(View v) {
+		switch(v.getId()){
+		
+			case R.id.begin_StopButton:
+				beginClick();
+				break;
+		}
+	}
+
+
+
+
+
+	private void beginClick() {
+		startActivity(new Intent("android.intent.action.Start_App"));
+	}
+
+}
---
+++ Android_SafetyTracker/src/com/example/android_safetytracker/Logs.java
@@ -0,0 +1,96 @@
+package com.example.android_safetytracker;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class Logs extends Activity {
+
+	Spinner logDropDown;
+	TextView text,textMiddle,textRight;
+	
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_logs);
+		
+		logDropDown = (Spinner) findViewById(R.id.log_DropDown);
+		text = (TextView)findViewById(R.id.tv);
+		
+		logDropDown = (Spinner) findViewById(R.id.log_DropDown);
+		textMiddle = (TextView)findViewById(R.id.tvMiddle);
+		
+		logDropDown = (Spinner) findViewById(R.id.log_DropDown);
+		textRight = (TextView)findViewById(R.id.tvRight);
+		
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.spinnerarray,
+				android.R.layout.simple_spinner_item);
+		
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		
+		
+		logDropDown.setAdapter(adapter);
+		logDropDown.setOnItemSelectedListener(new function());
+	}
+	
+	public class function implements OnItemSelectedListener {
+
+		@Override
+		public void onItemSelected(AdapterView<?> parent, View arg1, int pos,
+				long id) {
+			
+			if(pos ==0){
+				String str = parent.getItemAtPosition(pos).toString();
+				text.setText(" "+str);
+			
+				String str2 = parent.getItemAtPosition(pos+1).toString();
+				textMiddle.setText(" "+str2);
+			
+				String str3 = parent.getItemAtPosition(pos+2).toString();
+				textRight.setText(" "+str3);
+			}
+			else if(pos ==1){
+				String str = parent.getItemAtPosition(pos).toString();
+				text.setText(" "+str);
+			
+				String str2 = parent.getItemAtPosition(pos-1).toString();
+				textMiddle.setText(" "+str2);
+			
+				String str3 = parent.getItemAtPosition(pos+1).toString();
+				textRight.setText(" "+str3);
+			}
+			else{
+				String str = parent.getItemAtPosition(pos).toString();
+				text.setText(" "+str);
+			
+				String str2 = parent.getItemAtPosition(pos-2).toString();
+				textMiddle.setText(" "+str2);
+			
+				String str3 = parent.getItemAtPosition(pos-1).toString();
+				textRight.setText(" "+str3);
+			}
+		}
+
+		@Override
+		public void onNothingSelected(AdapterView<?> arg0) {
+			// TODO Auto-generated method stub
+
+		}
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.logs, menu);
+		return true;
+	}
+
+}
