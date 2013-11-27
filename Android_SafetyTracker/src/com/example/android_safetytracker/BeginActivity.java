4beb5f08526becc4ea4024807464b653cb9975a7
jrsilva <ruben_prod24@yahoo.com>
2013-11-27 15:32:09 -0800

    So we can mail

--- Android_SafetyTracker/src/com/example/android_safetytracker/BeginActivity.java
+++ Android_SafetyTracker/src/com/example/android_safetytracker/BeginActivity.java
@@ -16,6 +16,24 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+
+import android.widget.EditText;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 public class BeginActivity extends Activity implements View.OnClickListener, SensorEventListener{
 	
 	Button begin_Stop;
@@ -32,6 +50,13 @@ public class BeginActivity extends Activity implements View.OnClickListener, Sen
 	LinkedList<Event> linkedList;
 	int c = 0;
 	private Engine engine;
+	
+	
+	private static final String username = "carappfeedback@gmail.com";
+    private static final String password = "theinterns";
+    private EditText emailEdit;
+    private EditText subjectEdit;
+    private EditText messageEdit;
 	
 	
 	
@@ -56,6 +81,7 @@ public class BeginActivity extends Activity implements View.OnClickListener, Sen
 		engine = new Engine(this);
 		startService(new Intent(getBaseContext(), Engine.class));
 		
+		
 	}
 	
 	public void OnStart(){
@@ -69,6 +95,7 @@ public class BeginActivity extends Activity implements View.OnClickListener, Sen
 		sensorM.unregisterListener(this,accelerometer);
 		accelerometer = null;
 		stopService(new Intent(getBaseContext(), Engine.class));
+		//sendMail("carappfeedback@gmail.com","dd","ddd");
 
 		super.onStop();
 	}
@@ -295,5 +322,68 @@ public class BeginActivity extends Activity implements View.OnClickListener, Sen
 		}
 		
 	}
+	private void sendMail(String email, String subject, String messageBody) {
+        Session session = createSessionObject();
+
+        try {
+            Message message = createMessage(email, subject, messageBody, session);
+            new SendMailTask().execute(message);
+        } catch (AddressException e) {
+            e.printStackTrace();
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Message createMessage(String email, String subject, String messageBody, Session session) throws MessagingException, UnsupportedEncodingException {
+        Message message = new MimeMessage(session);
+        message.setFrom(new InternetAddress("carappfeedback@gmail.com", "theinterns"));
+        message.addRecipient(Message.RecipientType.TO, new InternetAddress(email, email));
+        message.setSubject(subject);
+        message.setText(messageBody);
+        return message;
+    }
+
+    private Session createSessionObject() {
+        Properties properties = new Properties();
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+        properties.put("mail.smtp.port", "587");
+
+        return Session.getInstance(properties, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password);
+            }
+        });
+    }
+
+    private class SendMailTask extends AsyncTask<Message, Void, Void> {
+      //  private ProgressDialog progressDialog;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+           // progressDialog = ProgressDialog.show(BeginActivity.this, "Please wait", "Sending mail", true, false);
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+           // progressDialog.dismiss();
+        }
+
+        @Override
+        protected Void doInBackground(Message... messages) {
+            try {
+                Transport.send(messages[0]);
+            } catch (MessagingException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
 	
 }
